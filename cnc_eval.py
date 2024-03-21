a15e8a0abc000f6cc787e09616f6936fc4656f4d
黃禹翔 <hyusterr>
2024-03-21 14:06:29 +0800

    add verbose

--- cnc_eval.py
+++ cnc_eval.py
@@ -2,8 +2,9 @@
 # 20220222_10-K_1090727_part2_item7_para100 Q0 20180221_10-K_1090727_part2_item7_para107 1 84.35596466064453 title-year2018_2022-filtered_out-cik1090727-year2022_2022-item7
 # output: evaluation metrics
 
-import pandas as pd
 import argparse
+import numpy as np
+import pandas as pd
 from collections import defaultdict
 from pathlib import Path
 from utils.utils import retrieve_paragraph_from_docid, read_jsonl
@@ -12,7 +13,7 @@ from evaluation.metrics import evaluate_a_pair_highlight
 from pprint import pprint
 
 
-def cnc_evaluate(retrieve_result_file, truth_file) -> dict:
+def cnc_evaluate(retrieve_result_file, truth_file, label_threshold=0.5, device='cpu', verbose=False) -> dict:
     # load the retrieve result to pandas dataframe
     truths = read_jsonl(truth_file)
 
@@ -27,7 +28,7 @@ def cnc_evaluate(retrieve_result_file, truth_file) -> dict:
     target_id_group['paragraph_list'] = target_id_group['doc_id_list'].apply(lambda x: [retrieve_paragraph_from_docid(doc_id) for doc_id in x])
 
     # load the highlighter
-    highlighter = CncBertHighlighter()
+    highlighter = CncBertHighlighter(device=device)
     
     # collect all the predictions
     predictions = dict()
@@ -40,27 +41,58 @@ def cnc_evaluate(retrieve_result_file, truth_file) -> dict:
                 target_text, 
                 paragraph_list,
                 mean_aggregate=True,
-                label_threshold=0.5,
+                label_threshold=label_threshold,
                 generate_spans=True
         )
         # add id info
         highlight_result['id'] = target_id
         predictions[target_id] = highlight_result
+        predictions[target_id]['references'] = paragraph_list
 
-
+       
     metrics = defaultdict(list)
     for truth in truths:
-        highlight_result = predictions[truth['id']]
+        target_id = truth['id']
+        highlight_result = predictions[target_id]
+        paragraph_list = highlight_result['references']
         # TODO: threshold setting duplicated, need to one of them
         # TODO: need to unify naming of keys
         metric_tmp = evaluate_a_pair_highlight(highlight_result, truth) #, threshold=0.5)
         for k, v in metric_tmp.items():
             if k != 'id':
                 metrics[k].append(v)
+
+        if verbose:
+            print(f"[target] id: {target_id}")
+            print(f"[target] text: {target_text}")
+            print("-"*50)
+            print(f"[highlight spans] truth:")
+            for span in truth["highlight_spans"]:
+                print(span)
+            print(f"[highlight spans] prediction:")
+            for span in highlight_result['highlight_spans_smooth']:
+                print(span)
+            print("-"*50)
+            print("[highlight prob. (mean)] truth:", np.array(truth['highlight_probs']).round(3))
+            print("[highlight prob. (mean)] prediction:", highlight_result['words_probs_tgt_mean'].round(3))
+            print("-"*50)
+            print(f"[references]: {len(paragraph_list)}")
+            for p in paragraph_list:
+                print(p)
+            print("-"*50)
+            print(f"metrics of this sample:")
+            pprint(metric_tmp)
+            print('+='*50)
+            print()
+
+
+
     # calculate the average
     for k, v in metrics.items():
         metrics[k] = sum(v) / len(v)
 
+    if verbose:
+        print(f"metrics of all samples:")
     pprint(metrics)
     return metrics
 
@@ -69,5 +101,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--retrieve_result_file', '-rf', type=str, help='the retrieve result file')
     parser.add_argument('--truth_file', '-tf', type=str, help='the truth file')
+    parser.add_argument('--label_threshold', '-lt', type=float, default=0.5, help='the threshold to use for label')
+    parser.add_argument('--device', '-d', type=str, default='cpu', help='the device to use')
+    parser.add_argument('--verbose', '-v', action='store_true', help='print the details')
     args = parser.parse_args()
-    cnc_evaluate(args.retrieve_result_file, args.truth_file)
+    cnc_evaluate(args.retrieve_result_file, args.truth_file, args.label_threshold, args.device, args.verbose)
