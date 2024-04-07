29799a25011e69d18489d5c7041dc6e0cce34015
黃禹翔 <hyusterr>
2024-04-07 21:21:51 +0800

    update changes

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from utils.utils import retrieve_paragraph_from_docid
 from highlighter.cnc_highlighter import CncBertHighlighter
+from highlighter.attn_highlighter import AttnHighlighter
 
 
 if __name__ == "__main__":
@@ -10,7 +11,11 @@ if __name__ == "__main__":
     ]
     text_references = [retrieve_paragraph_from_docid(docid) for docid in reference_docids]
     target = retrieve_paragraph_from_docid("20221028_10-K_320193_part2_item7_para7")
-    highlighter = CncBertHighlighter()
+    # highlighter = CncBertHighlighter()
+    # highlighter = AttnHighlighter(device='cuda:1')
+    # highlighter = AttnHighlighter('Sigma/financial-sentiment-analysis', 'text-classification', 'cuda:1')
+    # highlighter = AttnHighlighter('ahmedrachid/FinancialBERT-Sentiment-Analysis', 'text-classification', 'cuda:1')
+    highlighter = AttnHighlighter('mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis', 'text-classification', 'cuda:1')
     highlight_results = highlighter.highlighting_outputs(target, text_references, mean_aggregate=True, label_threshold=0.3, generate_spans=True)
     # highlighter.visualize_top_k_highlight(highlight_results, highlight_words_cnt=5)
     print(highlight_results)
