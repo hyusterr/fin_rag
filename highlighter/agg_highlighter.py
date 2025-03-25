7236ecb2f9e8de32e61bae6b76d60a8e7bb5f864
黃禹翔 <hyusterr>
2025-03-26 02:46:34 +0800

    add 3 types of aggHighighter

--- highlighter/agg_highlighter.py
+++ highlighter/agg_highlighter.py
@@ -1,41 +1,19 @@
 import torch
 
 from utils.utils import read_jsonl
+from data_utils import AGG_MAP
 from transformers import BertTokenizer, BertForTokenClassification, BertConfig
 from transformers.modeling_outputs import TokenClassifierOutput
 
-'''
-TRAINING_DATA = 'annotation/annotated_result/all/aggregate_train.jsonl'
-VALID_DATA = 'annotation/annotated_result/all/aggregate_test.jsonl'
-TEST_DATA = 'annotation/annotated_result/all/expert_annotated_test.jsonl'
-
-train_data = read_jsonl(TRAINING_DATA)
-valid_data = read_jsonl(VALID_DATA)
-test_data = read_jsonl(TEST_DATA)
-'''
-
 # option 1: mix all kinds of aggregation and apply different weights and train at the same time
-# option 2: at the beginning, only use one kind of aggregation and then gradually add more, let the model learn from deterministic to soft gradually
-'''
-class AggHighlighterDataset(torch.utils.data.Dataset):
-    def __init__(self, data, tokenizer):
-        self.data = data
-        self.tokenizer = tokenizer
-
-    def __getitem__(self, idx):
-        item = self.data[idx]
-        text = item['text']
-        agg_type = item['agg_type']
-        inputs = self.tokenizer(text, return_tensors='pt')
-        inputs['agg_type'] = agg_type
-        return inputs
-
-    def __len__(self):
-        return len(self.data)
-'''
+# option 2: at the beginning, only use one kind of aggregation and then gradually add more, let the model learn from deterministic to soft gradually --> cirriculum learning: strict --> loose
 # KEYS_TO_IGNORE_AT_INFERENCE = ['attentions', 'hidden_states']
 class AggHighlighter(BertForTokenClassification):
-    def __init__(self, config=None, agg_weights_base=0.7):
+    def __init__(self, config=None, agg_weights_base=0.7, strategy='mix', type_order=None):
+        assert strategy in ['mix', 'cirriculum', 'sequential']
+        for ty in type_order:
+            assert ty in AGG_MAP.keys()
+
         if config is None:
             config = BertConfig.from_pretrained('bert-base-uncased', num_labels=2) # keys_to_ignore_at_inference=KEYS_TO_IGNORE_AT_INFERENCE)
         super(AggHighlighter, self).__init__(config)
@@ -124,18 +102,3 @@ class AggHighlighter(BertForTokenClassification):
                 hidden_states=outputs.hidden_states,
                 attentions=outputs.attentions
             )
-
-
-    '''
-    def train(self):
-        pass
-
-    def predict(self, text):
-        input_ids = torch.tensor(self.tokenizer.encode(text)).unsqueeze(0)
-        with torch.no_grad():
-            outputs = self.model(input_ids)
-        logits = outputs[0]
-        _, predicted = torch.max(logits, 2)
-        predicted = predicted.squeeze(0)
-        return predicted
-    '''
