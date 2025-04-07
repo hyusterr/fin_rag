9c1b1930a9423ea6894e710b763f74e750beab1f
黃禹翔 <hyusterr>
2025-04-08 03:37:58 +0800

    fix syntax error

--- run.sh
+++ run.sh
@@ -12,8 +12,8 @@ python3 train_highlighter.py \
     --run_name $agg_type \
     --model_name bert-base-uncased \
     --train_agg_types $agg_type \
-    --metrics_for_best_model $early_stop_metric \
-    --greater_is_better $greater_is_better \
+    --metric_for_best_model $early_stop_metric \
+    --greater_is_better $greater_is_better 
 done
 done
 
@@ -37,17 +37,17 @@ python3 train_highlighter.py \
     --run_name iterate_$agg_type-$second_agg_type \
     --model_name bert-base-uncased \
     --train_agg_types $agg_type
-    --metrics_for_best_model $early_stop_metric \
-    --greater_is_better $greater_is_better \
+    --metric_for_best_model $early_stop_metric \
+    --greater_is_better $greater_is_better 
 
 python3 train_highlighter.py \
     --output_dir ./checkpoints/iterate_$agg_type-$second_agg_type \
     --run_name iterate_$agg_type-$second_agg_type \
     --model_name bert-base-uncased \
     --train_agg_types $second_agg_type \
-    --metrics_for_best_model $early_stop_metric \
+    --metric_for_best_model $early_stop_metric \
     --greater_is_better $greater_is_better \
-    --resume_from_checkpoint True \
+    --resume_from_checkpoint True 
 
 # curriculum training
 python3 train_highlighter.py \
@@ -55,7 +55,7 @@ python3 train_highlighter.py \
     --run_name curriculum_$agg_type-$second_agg_type \
     --model_name agg_highlighter \
     --train_agg_types $agg_type $second_agg_type \
-    --metrics_for_best_model $early_stop_metric \
+    --metric_for_best_model $early_stop_metric \
     --greater_is_better $greater_is_better \
     --agg_strategy curriculum \
     --agg_type_order $agg_type $second_agg_type 
@@ -66,7 +66,7 @@ python3 train_highlighter.py \
     --run_name mix_$agg_type-$second_agg_type \
     --model_name agg_highlighter \
     --train_agg_types $agg_type $second_agg_type \
-    --metrics_for_best_model $early_stop_metric \
+    --metric_for_best_model $early_stop_metric \
     --greater_is_better $greater_is_better \
     --agg_type_weights 0.5 \
     --agg_strategy mix \
@@ -101,11 +101,11 @@ else
     greater_is_better="False"
 fi
 python3 train_highlighter.py \
-    --output_dir ./checkpoints/cnc_$agg_type \
-    --run_name cnc_$agg_type \
+    --output_dir ./checkpoints/cnc_train_$agg_type \
+    --run_name cnc_train_$agg_type \
     --model_name cnc_highlighter \
     --train_agg_types $agg_type \
-    --metrics_for_best_model $early_stop_metric \
+    --metric_for_best_model $early_stop_metric \
     --greater_is_better $greater_is_better 
 done
 done
